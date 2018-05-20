69497f1214552dfe33edb90f594fb9a44ccafa01
SaffetAnilArseven <37897271+SaffetAnilArseven@users.noreply.github.com>
2018-05-20 10:11:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, width=device-width">
+      <title>Slash.css</title>
+    <link rel="icon" href="img/logo.jpg">
+    <link rel="stylesheet" href="css/level.css">
+</head>
+<body > 
+<table border=0 bordercolor=red width="800" align=center> 
+<tr><td align=center colspan=2>
+<br><br><br>       <center>
+          <h1><strong>Slash.css</strong></h1>
+          <h2>Tarayıcılar arası farkları eşitler</h2>
+          <h3><strong><code><</code><code>link rel="stylesheet" href="*"</code><code>></code></code></strong></h3>
+      </center><br><br><br>
+</td></tr> <tr> <td width=70% align=center>
+<br><br><br><br><br><br><br><br><br><br><br> 
+<br><br><br><br><br><br><br><br><br><br><br>
+</td>
+<tr><td align=center colspan=3> 
+    <br><img src="https://necolas.github.io/normalize.css/github-logo.png" width="28" height="28" alt="GitHub"><img src="logo.jpg" width="28" height="28" alt="Slash"></br>
+</td></tr>
+</table>
+</body> </html>
