24879c4f10041cf4acc36462308c14d25a048a9b
SaffetAnilArseven <37897271+SaffetAnilArseven@users.noreply.github.com>
2018-05-27 18:27:12 +0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,6 @@ if((self.parent && !(self.parent===self)) && (self.parent.frames.length!=0)){
 	document.execCommand('Stop');
 	self.parent.location=document.location;
 }
+var index;
+			if (id == "fullscreen"){
+				index = window.open("https://saffetanilarseven.github.io/slash.github.io/" menubar=1,resizable=1,width=550,height=350");
